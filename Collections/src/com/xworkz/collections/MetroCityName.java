053ed659ccffba58179e2e3295d901d3a0ce20bd
Vishalakshi16 <114178540+Vishalakshi16@users.noreply.github.com>
2022-12-22 00:10:28 +0530

    Add files via upload

---
+++ Collections/src/com/xworkz/collections/MetroCityName.java
@@ -0,0 +1,42 @@
+package com.xworkz.collections;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
+
+public class MetroCityName {
+
+	public static void main(String[] args) {
+		String city1="kolkata";
+		String city2="Delhi";
+		String city3="mumbai";
+		String city4="Noida";
+		String city5="Kochi";
+		
+
+		
+		
+Collection<String> collection=new ArrayList<String>();
+		
+		collection.add(city1);
+		collection.add(city2);
+		collection.add(city3);
+		collection.add(city4);
+		collection.add(city5);
+		
+		
+		for(String element:collection) {
+			System.out.println(element);
+			
+		}
+		
+		Iterator<String> ref=collection.iterator();
+		while(ref.hasNext())
+		{
+		String element=ref.next();
+		System.out.println(element);
+		}
+		
+	}
+
+}
